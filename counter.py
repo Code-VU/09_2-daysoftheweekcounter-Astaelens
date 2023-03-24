7a0251d22a8b4c388deb32259bf76753d0256cb3
Alex Staelens <Alex.Staelens@vu.com>
2023-03-24 14:53:37 -0500

    created for look to parse lines and if statement to pull out day of the week

--- counter.py
+++ counter.py
@@ -2,7 +2,18 @@ def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
 
+    counter=dict()
+    fhand= open(file_name)
+    for line in fhand:
+        if line.startswith('From '):
+            word=line.split()
+            #print(word)
+            if word[2] not in counter:
+                counter[word[2]]=1
+            else:
+                counter[word[2]]+=1
 
+    print(counter)
 
 ## if you want to test locally run > python payCalculator.py
 if __name__ == "__main__":
